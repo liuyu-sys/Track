fffb71729366517c31d6ef88663d44a002d10138
liuyu <2543722345@qq.com>
2023-05-22 20:46:25 +0800

    add wgs84 to gcj02

---
+++ components/convert_lat_lng/convert_lat_lng.c
@@ -0,0 +1,134 @@
+#include "convert_lat_lng.h"
+
+#define CONSTRAIN(value, minValue, maxValue) ((value < minValue) ? (minValue) : ((value > maxValue) ? (maxValue) : (value)))
+
+unsigned long mKeyPath;
+unsigned long mKeyName;
+uint8_t mKeyExt;
+uint8_t pixelSubX = 0, pixelSubY = 0;
+const unsigned long EarthRadius = 6378137;
+const double MinLatitude = -85.05112878;
+const double MaxLatitude = 85.05112878;
+const double MinLongitude = -180.0;
+const double MaxLongitude = 180.0;
+const uint8_t MaxLevel = 14;
+const uint8_t MinLevel = 2;
+
+// 计算经度（longitude）对应的瓦片坐标 X
+double calculateTileX(double longitude, int zoom)
+{
+    return ((longitude + 180.0) / 360.0 * pow(2, zoom));
+}
+
+// 计算纬度（latitude）对应的瓦片坐标 Y
+int calculateTileY(double latitude, int zoom)
+{
+    double latRad = latitude * M_PI / 180.0;
+    return ((1.0 - log(tan(latRad) + 1.0 / cos(latRad)) / M_PI) / 2.0 * pow(2, zoom));
+}
+
+static double Clip(double n, double minValue, double maxValue)
+{
+    return CONSTRAIN(n, minValue, maxValue);
+}
+
+static unsigned long ClipULONG(unsigned long n, unsigned long minValue, unsigned long maxValue)
+{
+    return CONSTRAIN(n, minValue, maxValue);
+}
+
+static int ClipLevel(uint8_t n)
+{
+    return CONSTRAIN(n, MinLevel, MaxLevel);
+}
+
+static unsigned long MapSize(int levelOfDetail)
+{
+    unsigned long seed = 256;
+    return seed << levelOfDetail;
+}
+
+double transformlng(double lng, double lat)
+{
+    double ret = 300.0 + lng + 2.0 * lat + 0.1 * lng * lng + 0.1 * lng * lat + 0.1 * sqrt(fabs(lng));
+    ret += (20.0 * sin(6.0 * lng * M_PI) + 20.0 * sin(2.0 * lng * M_PI)) * 2.0 / 3.0;
+    ret += (20.0 * sin(lng * M_PI) + 40.0 * sin(lng / 3.0 * M_PI)) * 2.0 / 3.0;
+    ret += (150.0 * sin(lng / 12.0 * M_PI) + 300.0 * sin(lng / 30.0 * M_PI)) * 2.0 / 3.0;
+    return ret;
+}
+
+double transformlat(double lng, double lat)
+{
+    double ret = -100.0 + 2.0 * lng + 3.0 * lat + 0.2 * lat * lat + 0.1 * lng * lat + 0.2 * sqrt(fabs(lng));
+    ret += (20.0 * sin(6.0 * lng * M_PI) + 20.0 * sin(2.0 * lng * M_PI)) * 2.0 / 3.0;
+    ret += (20.0 * sin(lat * M_PI) + 40.0 * sin(lat / 3.0 * M_PI)) * 2.0 / 3.0;
+    ret += (160.0 * sin(lat / 12.0 * M_PI) + 320.0 * sin(lat * M_PI / 30.0)) * 2.0 / 3.0;
+    return ret;
+}
+void wgs84_to_gcj02(double *lng, double *lat)
+{
+    double dlat, dlng, magic, sqrtmagic, radlat;
+    dlat = transformlat(*lng - 105.0, *lat - 35.0);
+    dlng = transformlng(*lng - 105.0, *lat - 35.0);
+    radlat = *lat / 180.0 * M_PI;
+    magic = sin(radlat);
+    magic = 1 - EE * magic * magic;
+    sqrtmagic = sqrt(magic);
+    dlat = (dlat * 180.0) / ((A * (1 - EE)) / (magic * sqrtmagic) * M_PI);
+    dlng = (dlng * 180.0) / (A / sqrtmagic * cos(radlat) * M_PI);
+    *lat = *lat + dlat;
+    *lng = *lng + dlng;
+}
+
+static void LatLongToPixelXY(double latitude, double longitude, int levelOfDetail, unsigned long *pixelX, unsigned long *pixelY)
+{
+    latitude = Clip(latitude, MinLatitude, MaxLatitude);
+    longitude = Clip(longitude, MinLongitude, MaxLongitude);
+
+    double x = (longitude + 180.0) / 360.0;
+    double sinLatitude = sin(latitude * M_PI / 180.0);
+    double y = 0.5 - log((1.0 + sinLatitude) / (1.0 - sinLatitude)) / (4.0 * M_PI);
+
+    unsigned long mapSize = MapSize(levelOfDetail);
+    *pixelX = ClipULONG(x * mapSize + 0.5, 0, mapSize - 1);
+    *pixelY = ClipULONG(y * mapSize + 0.5, 0, mapSize - 1);
+}
+static void PixelXYToTileXY(unsigned long pixelX, unsigned long pixelY, int screenWidth, int screenHeigh, unsigned long *tileX, unsigned long *tileY,
+                            uint8_t *tileSubX, uint8_t *tileSubY, uint8_t *pSubX, uint8_t *pSubY)
+{
+    *tileX = pixelX / 256;
+    *tileY = pixelY / 256;
+    *tileSubX = pixelX % 256 / screenWidth;
+    *tileSubY = pixelY % 256 / screenHeigh;
+    *pSubX = pixelX % 256 % screenWidth;
+    *pSubY = pixelY % 256 % screenHeigh;
+}
+
+int TEST_main(int argc)
+{
+    double longitude = 112.924530; // 经度 27.850729" lon="112.916763
+    double latitude = 27.847441;   // 纬度 27.847441" lon="112.924530
+    int zoom = 18;                 // 缩放级别
+    wgs84_to_gcj02(&longitude, &latitude);
+    double tileX_double = calculateTileX(longitude, zoom);
+    double tileY_double = calculateTileY(latitude, zoom);
+    printf("tileX_double = %d, tileY_double = %d\n", (int)tileX_double, (int)tileY_double);
+    unsigned long tileX, tileY;
+
+    unsigned long PixelX, PixelY;
+    LatLongToPixelXY(latitude, longitude, zoom, &PixelX, &PixelY);
+
+    uint8_t tileSubX, tileSubY;
+
+    PixelXYToTileXY(PixelX, PixelY, 256, 256, &tileX, &tileY, &tileSubX, &tileSubY, &pixelSubX, &pixelSubY);
+    printf("path: D:\\lvgl_study\\map\\%d\\%ld\\%ld\n", zoom, tileX, tileY);
+
+    printf("Pixel X: %ld\n", PixelX);
+    printf("Pixel Y: %ld\n", PixelY);
+    printf("tileSub X: %d\n", tileSubX);
+    printf("tileSub Y: %d\n", tileSubY);
+    printf("pixelSub X: %d\n", pixelSubX);
+    printf("pixelSub Y: %d\n", pixelSubY);
+
+    return 0;
+}
---
+++ components/convert_lat_lng/include/convert_lat_lng.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <math.h>
+#include <stdio.h>
+#include <stdint.h>
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+#define X_PI (M_PI * 3000.0 / 180.0)
+#define A (6378245.0)               // #长半轴
+#define EE (0.00669342162296594323) // #偏心率平方
+
+static void PixelXYToTileXY(unsigned long pixelX, unsigned long pixelY, int screenWidth, int screenHeigh, unsigned long *tileX, unsigned long *tileY,
+                            uint8_t *tileSubX, uint8_t *tileSubY, uint8_t *pSubX, uint8_t *pSubY);
+static void LatLongToPixelXY(double latitude, double longitude, int levelOfDetail, unsigned long *pixelX, unsigned long *pixelY);
+
+void wgs84_to_gcj02(double *lng, double *lat);
